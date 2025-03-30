ce94c82b82e556487c7ce4e47b8068b3b0d036ee
ohgus <anakin1108@naver.com>
2025-03-31 00:51:09 +0900

    Updates

--- assets/index-C7ZxYPAj.js
+++ assets/index-C7ZxYPAj.js
@@ -235,23 +235,23 @@ const MyRate = (id) => {
     <span class="rate-stars">
       <input type="radio" name="rate" id="rate1" value="2" class="star-icon-radio" />
       <label for="rate1">
-        <img class="star-icon" src="/images/star_${userRate >= 2 ? "filled" : "empty"}.png" />
+        <img class="star-icon" src="images/star_${userRate >= 2 ? "filled" : "empty"}.png" />
       </label>
       <input type="radio" name="rate" id="rate2" value="4" class="star-icon-radio" />
       <label for="rate2">
-        <img class="star-icon" src="/images/star_${userRate >= 4 ? "filled" : "empty"}.png" />
+        <img class="star-icon" src="images/star_${userRate >= 4 ? "filled" : "empty"}.png" />
       </label>
       <input type="radio" name="rate" id="rate3" value="6" class="star-icon-radio" />
       <label for="rate3">
-        <img class="star-icon" src="/images/star_${userRate >= 6 ? "filled" : "empty"}.png" />
+        <img class="star-icon" src="images/star_${userRate >= 6 ? "filled" : "empty"}.png" />
       </label>
       <input type="radio" name="rate" id="rate4" value="8" class="star-icon-radio" />
       <label for="rate4">
-        <img class="star-icon" src="/images/star_${userRate >= 8 ? "filled" : "empty"}.png" />
+        <img class="star-icon" src="images/star_${userRate >= 8 ? "filled" : "empty"}.png" />
       </label>
       <input type="radio" name="rate" id="rate5" value="10" class="star-icon-radio" />
       <label for="rate5">
-        <img class="star-icon" src="/images/star_${userRate >= 10 ? "filled" : "empty"}.png" />
+        <img class="star-icon" src="images/star_${userRate >= 10 ? "filled" : "empty"}.png" />
       </label>
     </span>
     <p class="rate-text">
@@ -279,7 +279,7 @@ function updateMyRateStar(newRate) {
   if (!stars) return;
   stars.forEach((star, index) => {
     const rate = (index + 1) * 2;
-    star.src = `/images/star_${rate <= newRate ? "filled" : "empty"}.png`;
+    star.src = `images/star_${rate <= newRate ? "filled" : "empty"}.png`;
   });
 }
 const updateMyRateText = (rate) => {
